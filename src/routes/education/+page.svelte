cc69a684e19ab1a2198088cda80e555ebe07e8c4
Auxtal <real.auxtal@gmail.com>
2022-12-16 10:19:45 +0800

    fix: :bug: Fixed education page on small screens

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -23,36 +23,38 @@
 	<title>Education</title>
 </svelte:head>
 
-<div class="flex h-screen items-center">
-	<Timeline position="alternate">
-		{#each Education as Education, i}
-			<TimelineItem style="margin:15px 0">
-				<TimelineSeparator>
-					{#if animate}
-						<span in:fly|local={{ delay: 250, y: (-100 / 2) * (i + 1), duration: 1500 }}>
-							<TimelineDot
-								style={`background-color: ${
-									i % 2 == 0 ? "var(--accent)" : "var(--light)"
-								}; border: none;`}
-							/>
-						</span>
-					{/if}
-					<TimelineConnector />
-				</TimelineSeparator>
-				<TimelineContent>
-					{#if animate}
-						<h3 in:fly|local={{ x: -150, duration: 2000 }} class="font-secondary !font-extrabold">
-							{Education.place}
-						</h3>
-						<p in:fly|local={{ x: 100, duration: 1500 }} class="py-2 text-sm text-neutral">
-							{Education.year_start + "-" + Education.year_end}
-						</p>
-						<p in:fade|local={{ delay: 1500, duration: 1500 }} class="font-secondary text-accent">
-							{Education.description}
-						</p>
-					{/if}
-				</TimelineContent>
-			</TimelineItem>
-		{/each}
-	</Timeline>
+<div class="flex h-screen items-center justify-center">
+	<div class="max-h-full w-full sm:max-h-[36rem]">
+		<Timeline position="alternate">
+			{#each Education as Education, i}
+				<TimelineItem style="margin:15px 0">
+					<TimelineSeparator>
+						{#if animate}
+							<span in:fly|local={{ delay: 250, y: (-100 / 2) * (i + 1), duration: 1500 }}>
+								<TimelineDot
+									style={`background-color: ${
+										i % 2 == 0 ? "var(--accent)" : "var(--light)"
+									}; border: none;`}
+								/>
+							</span>
+						{/if}
+						<TimelineConnector />
+					</TimelineSeparator>
+					<TimelineContent>
+						{#if animate}
+							<h3 in:fly|local={{ x: -150, duration: 2000 }} class="font-secondary !font-extrabold">
+								{Education.place}
+							</h3>
+							<p in:fly|local={{ x: 100, duration: 1500 }} class="py-2 text-sm text-neutral">
+								{Education.year_start + "-" + Education.year_end}
+							</p>
+							<p in:fade|local={{ delay: 1500, duration: 1500 }} class="font-secondary text-accent">
+								{Education.description}
+							</p>
+						{/if}
+					</TimelineContent>
+				</TimelineItem>
+			{/each}
+		</Timeline>
+	</div>
 </div>
