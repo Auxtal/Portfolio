68082a3bade863945801581c9822ecccb2f7b125
Auxtal <real.auxtal@gmail.com>
2023-01-18 23:49:16 +0800

    perf: :arrow_up: Added cssnano postcss plugin to minify css output

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
-	plugins: {
-		autoprefixer: {},
-		tailwindcss: {}
-	}
-};
+	plugins: [
+		require('autoprefixer'),
+		require('cssnano')({
+			preset: 'default'
+		}),
+		require('tailwindcss')
+	]
+}
