7d4f1d273a66ca88058f302662efad0e212f5e9f
Auxtal <real.auxtal@gmail.com>
2023-01-19 14:30:05 +0800

    refactor: :lipstick: Updated education page transition settings

--- src/routes/education/+page.svelte
+++ src/routes/education/+page.svelte
@@ -19,50 +19,55 @@
 	<title>Education</title>
 </svelte:head>
 
-<div class="flex h-screen items-center justify-center">
-	<div class="absolute top-20 w-full lg:top-28 xl:relative xl:top-0">
-		<Timeline position="alternate">
-			{#each Education as Education, i}
-				<TimelineItem style="margin:15px 0">
-					<TimelineSeparator>
-						<Animate>
-							<span
-								transition:fly|local={{
-									delay: 100,
-									y: (-100 / 2) * (i + 1),
-									duration: 500,
-									easing: quintOut
-								}}
-							>
-								<TimelineDot style={`background-color: var(--neutral); border: none;`} />
-							</span>
-						</Animate>
-						<TimelineConnector />
-					</TimelineSeparator>
-					<TimelineContent>
-						<Animate>
-							<h3
-								transition:fly|local={{ x: -150, duration: 500, easing: quintOut }}
-								class="font-secondary font-extrabold"
-							>
-								{Education.place}
-							</h3>
-							<p
-								transition:slide|local={{ delay: 250, duration: 500, easing: quintOut }}
-								class="py-2 text-sm text-neutral"
-							>
-								{Education.year_start} - {Education.year_end}
-							</p>
-							<p
-								transition:fade|local={{ delay: 500, duration: 800, easing: quintOut }}
-								class="font-secondary text-accent"
-							>
-								{Education.description}
-							</p>
-						</Animate>
-					</TimelineContent>
-				</TimelineItem>
-			{/each}
-		</Timeline>
+<Animate>
+	<div class="flex h-screen items-center justify-center">
+		<div
+			class="absolute top-20 w-full lg:top-28 xl:relative xl:top-0"
+			transition:fade|local={{ duration: 500 }}
+		>
+			<Timeline position="alternate">
+				{#each Education as Education, i}
+					<TimelineItem style="margin:15px 0">
+						<TimelineSeparator>
+							<Animate>
+								<span
+									transition:fly|local={{
+										delay: 50,
+										y: (-100 / 2) * (i + 1),
+										duration: 500,
+										easing: quintOut
+									}}
+								>
+									<TimelineDot style={`background-color: var(--neutral); border: none;`} />
+								</span>
+							</Animate>
+							<TimelineConnector />
+						</TimelineSeparator>
+						<TimelineContent>
+							<Animate>
+								<h3
+									transition:fly|local={{ x: -150, duration: 500, easing: quintOut }}
+									class="font-secondary font-extrabold"
+								>
+									{Education.place}
+								</h3>
+								<p
+									transition:slide|local={{ delay: 150, duration: 500, easing: quintOut }}
+									class="py-2 text-sm text-neutral"
+								>
+									{Education.year_start} - {Education.year_end}
+								</p>
+								<p
+									transition:fade|local={{ delay: 150, duration: 800, easing: quintOut }}
+									class="font-secondary text-accent"
+								>
+									{Education.description}
+								</p>
+							</Animate>
+						</TimelineContent>
+					</TimelineItem>
+				{/each}
+			</Timeline>
+		</div>
 	</div>
-</div>
+</Animate>
