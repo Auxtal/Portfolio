013328d4e78984085ed0bdfdd2a3738cac250c3c
Auxtal <ethan@auxtal.xyz>
2023-01-27 12:39:25 +0800

    style: :art: Updated formatting

--- src/lib/helpers/toast.ts
+++ src/lib/helpers/toast.ts
@@ -3,24 +3,26 @@ import toast from "svelte-french-toast";
 export const Toast = {
   success: (message: string) => {
     toast.success(message, {
-      style: `background-color: rgba(var(--secondary-rgb), 0.2);
-							backdrop-filter: blur(8px);
-							color: var(--secondary);
-							transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-							transition-property: background-color, color;
-							transition-duration: 150ms;`,
+      style: `
+        background-color: rgba(var(--secondary-rgb), 0.2);
+        backdrop-filter: blur(8px);
+        color: var(--secondary);
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-property: background-color, color;
+        transition-duration: 150ms;`,
       duration: 5000,
       position: "bottom-right"
     });
   },
   error: (message: string) => {
     toast.error(message, {
-      style: `background-color: rgba(var(--secondary-rgb), 0.2);
-							backdrop-filter: blur(8px);
-							color: var(--secondary);
-							transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-							transition-property: background-color, color;
-							transition-duration: 150ms;`,
+      style: `
+        background-color: rgba(var(--secondary-rgb), 0.2);
+        backdrop-filter: blur(8px);
+        color: var(--secondary);
+        transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+        transition-property: background-color, color;
+        transition-duration: 150ms;`,
       duration: 5000,
       position: "bottom-right"
     });
