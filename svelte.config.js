0b98d5e9f090f0ed933c69011ef336cb1ce31633
Auxtal <ethan@auxtal.xyz>
2023-03-27 15:26:28 +0800

    refactor: :sparkles: Setup cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,18 @@ import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
 
 import sveltePreprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-netlify";
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
   },
   preprocess: [sveltePreprocess(), mdsvex(mdsvexConfig)]
 };
