0fb7b0ba449abc1c6c2d305245e7e82748cb213e
Auxtal <ethan@auxtal.xyz>
2023-05-31 15:27:49 +0800

    fix: :bug: Maybe this will work

--- src/lib/utils/types.ts
+++ src/lib/utils/types.ts
@@ -6,19 +6,7 @@ export type Post = {
   published: string;
   updated: string;
   tags: string[];
-  cover: any;
   readingTime: string;
   hidden: boolean;
+  render: () => { html: string };
 };
-
-export interface Metadata {
-  id: number;
-  layout: string;
-  slug: string;
-  title: string;
-  published: string;
-  updated: string;
-  tags: string[];
-  readingTime: string;
-  hidden: boolean;
-}
--- src/routes/blog/posts/+layout.server.ts
+++ src/routes/blog/posts/+layout.server.ts
@@ -1,4 +1,8 @@
+import readingTime from "reading-time";
+import striptags from "striptags";
+
 import { importPosts } from "$lib/utils/posts/server";
+import type { Post } from "$lib/utils/types";
 
 export const load = async ({ url }: { url: { pathname: string } }) => {
   const { pathname } = url;
@@ -7,7 +11,20 @@ export const load = async ({ url }: { url: { pathname: string } }) => {
     return posts.find((post) => post.slug === slug);
   });
 
+  const loadReadingTime = (post: Post) => {
+    const html = post.render()?.html;
+    const readingTimeResult = html ? readingTime(striptags(html) || "") : undefined;
+    return readingTimeResult;
+  };
+
+  // Removes the render function from the post object for the client
+  const postNoRender = JSON.parse(JSON.stringify(post));
+  Reflect.deleteProperty(postNoRender, "render");
+
   return {
-    post
+    post: postNoRender,
+    streams: {
+      readingTime: loadReadingTime(post)
+    }
   };
 };
