04d371e5e8d45630ff9039cddcabe08590105a2a
Auxtal <ethan@auxtal.xyz>
2023-07-07 12:29:25 +0800

    fix: :lipstick: Fixed selected tag styling

--- src/lib/components/atoms/Tag.svelte
+++ src/lib/components/atoms/Tag.svelte
@@ -5,14 +5,17 @@
   export let selectedTag: string | undefined = undefined;
   export let name: string;
 
-  $: highlightTag = clsx({ "!bg-neutral": name.toLowerCase() === selectedTag?.toLowerCase() });
+  $: highlightTag = clsx({
+    "bg-neutral text-primary font-bold dark:text-secondary":
+      name.toLowerCase() === selectedTag?.toLowerCase()
+  });
   const handleTagClick = () => (selectedTag = name);
 </script>
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <div
   class={twMerge(
-    "tag mx-1 mt-2 inline-block cursor-pointer rounded bg-secondary/10 p-2 px-4 text-secondary transition-colors first:ml-0 hover:bg-neutral sm:mt-0",
+    "tag mx-1 mt-2 inline-block cursor-pointer rounded bg-secondary/10 p-2 px-4 text-secondary transition-colors first:ml-0 hover:bg-neutral hover:text-primary hover:dark:text-secondary sm:mt-0",
     highlightTag
   )}
   on:click={handleTagClick}
